a8ae97ec637c765aeacfbdafd4d6f9fbcf7a6c44
PabloSFlores <105692302+PabloSFlores@users.noreply.github.com>
2023-01-12 00:28:55 -0600

    Petición de gifs
    Muestra los gifs en cards

---
+++ AppWebI4/js/funciones.js
@@ -0,0 +1,31 @@
+const inputSearch = document.getElementById("search");
+const api_key = 'wmv0INF90WkqHl8pj3I1j2IePnWfFsG7'
+
+// inputSearch.addEventListener("keypress", function(event) {
+//   if (event.key === "Enter") {
+//     event.preventDefault();
+//     alert("Hola")
+//   }
+// });
+
+inputSearch.onkeyup = async (event) => {
+    var imgContainer = document.getElementById("imgContainer")
+    event.preventDefault();
+    if (event.keyCode !== 13) return;
+    imgContainer.innerHTML = ''
+    const req = await fetch(
+        `http://api.giphy.com/v1/gifs/search?api_key=${api_key}&q=${inputSearch.value}`
+    );
+    const response = await req.json();
+    console.log(response);
+    for (let i = 0; i < response.data.length; i++) {
+        var imgUrl = response.data[i].images.original.url;
+        // console.log(imgUrl);
+        var imgList = `<div class="col-2 shadow p-2 mb-3 ml-3 mr-3 bg-white rounded">
+                        <img src="${imgUrl}" width="100%" high="100%">
+                        </div>
+                        `
+        imgContainer.innerHTML += imgList;
+    }
+    // console.log(imgContainer.innerHTML);
+}
